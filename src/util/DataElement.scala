996025fdde87e3ff8c2f75fe14c522b91920fabf
Zackery-Kiyoshi <zkurimab@trinity.edu>
2016-03-31 15:34:36 -0500

    forgot to commit?????

--- src/util/DataElement.scala
+++ src/util/DataElement.scala
@@ -1,6 +1,8 @@
 package util
 
-class DataElement(in:Vector[Double]) {
+class DataElement(in:Vector[Double]) extends Container() {
+  val inputs: Int = 0
+  val outputs: Int = 0
   private var element:Array[Double] = in.to[Array] 
   // scala.collection.immutable.IndexedSeq[Double]
   def get():Vector[Double] ={
@@ -11,5 +13,6 @@ class DataElement(in:Vector[Double]) {
   def set(t:Vector[Double]){
     element = t.to[Array]
   }
+  
   
 }
