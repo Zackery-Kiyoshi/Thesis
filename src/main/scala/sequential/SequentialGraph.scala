97f95593684d4eff0da94afc42b2f939e0ad6cb8
Zackery-Kiyoshi <zkurimab@trinity.edu>
2017-04-14 17:27:25 -0500

    fixed printing

--- src/main/scala/sequential/SequentialGraph.scala
+++ src/main/scala/sequential/SequentialGraph.scala
@@ -18,36 +18,38 @@ class SequentialGraph (
   override val nextfkey: Int,
   override val nextdkey: Int,
   override val runOnModify:Boolean,
-  override val parent:WeakReference[SequentialGraph]
+  override val parent:WeakReference[SequentialGraph],
+  val print:Boolean=false
   ) extends Graph(filtKeys,fKeys,dataKeys,dKeys,funcToData,dataToFunc,funcToInputs,nextfkey,nextdkey,runOnModify,parent){
   
 //  private val runOnModify = true
   private var running = true
-  private var print = false
-  def setPrints(b:Boolean){print=b}
+  def setPrints(b:Boolean){
+    new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, dataToFunc,funcToInputs, nextfkey, nextdkey,runOnModify,WeakReference(this),b)
+  }
   
   override def setInput(f:FKey,newInputs:Vector[DKey]):SequentialGraph={
-    new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, dataToFunc,funcToInputs+(f->newInputs), nextfkey, nextdkey,runOnModify,WeakReference(this))
+    new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, dataToFunc,funcToInputs+(f->newInputs), nextfkey, nextdkey,runOnModify,WeakReference(this),print)
   }
   override def setInput(f:String,newInputs:Vector[DKey]):SequentialGraph={
     var ret = setInputHelper(getFKey(f),newInputs)
-    return new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, dataToFunc, ret, nextfkey, nextdkey, runOnModify, WeakReference(this))
+    return new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, dataToFunc, ret, nextfkey, nextdkey, runOnModify, WeakReference(this),print)
   }
   
   override def replace(fstr: String, f2: Filter): SequentialGraph = {
     var ret = replaceHelper(fstr,f2)
-    return new SequentialGraph(ret._1, fKeys, ret._2, dKeys, funcToData, dataToFunc, funcToInputs, nextfkey, nextdkey, runOnModify, WeakReference(this))
+    return new SequentialGraph(ret._1, fKeys, ret._2, dKeys, funcToData, dataToFunc, funcToInputs, nextfkey, nextdkey, runOnModify, WeakReference(this),print)
   }
   
   override def modify(fstr: String)(func: Filter => Filter):SequentialGraph = {
     var ret = modifyHelper(fstr)(func)
-    return new SequentialGraph(ret._1, fKeys, ret._2, dKeys, funcToData, dataToFunc, funcToInputs, nextfkey, nextdkey, runOnModify, WeakReference(this))
+    return new SequentialGraph(ret._1, fKeys, ret._2, dKeys, funcToData, dataToFunc, funcToInputs, nextfkey, nextdkey, runOnModify, WeakReference(this),print)
   }
   
   override def addFilter(filter:Filter, fName: String = ""): SequentialGraph = {
     var ret = addFilterHelper(filter, fName)
     
-    return new SequentialGraph(ret._1, ret._2, ret._3, ret._4, ret._5, ret._6, ret._7, ret._8, ret._9, runOnModify, WeakReference(this))
+    return new SequentialGraph(ret._1, ret._2, ret._3, ret._4, ret._5, ret._6, ret._7, ret._8, ret._9, runOnModify, WeakReference(this),print)
   }
 
   override def addFilter(filter:Filter): SequentialGraph = {
@@ -56,7 +58,7 @@ class SequentialGraph (
  
   override def connectNodes(d: DKey, f: FKey): SequentialGraph = {
     var ret = connectNodesHelper(d,f)
-    return new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, ret._1, ret._2, nextfkey, nextdkey, runOnModify, WeakReference(this))
+    return new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, ret._1, ret._2, nextfkey, nextdkey, runOnModify, WeakReference(this),print)
   }
 
   override def connectNodes(d:String, f:String,idx:Int=0): SequentialGraph = {
@@ -66,7 +68,7 @@ class SequentialGraph (
   
   override def disconnectNodes(d: DKey, f: FKey): SequentialGraph = {
     var ret = disconnectNodesHelper(d,f)
-    return new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, ret._1, ret._2, nextfkey, nextdkey, runOnModify, WeakReference(this))
+    return new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, ret._1, ret._2, nextfkey, nextdkey, runOnModify, WeakReference(this),print)
   }
 
   override def disconnectNodes(d:String, f:String,idx:Int=0): SequentialGraph = {
@@ -75,7 +77,7 @@ class SequentialGraph (
   
   override def removeNode(f: FKey): SequentialGraph = {
     var ret = removeNodeHelper(f)
-    return new SequentialGraph(ret._1, ret._2, ret._3, ret._4, ret._5, ret._6, ret._7, nextfkey, nextdkey, runOnModify, WeakReference(this))
+    return new SequentialGraph(ret._1, ret._2, ret._3, ret._4, ret._5, ret._6, ret._7, nextfkey, nextdkey, runOnModify, WeakReference(this),print)
   }
   override def removeNode(f: String): SequentialGraph = {
     removeNode(getFKey(f))
@@ -145,7 +147,7 @@ class SequentialGraph (
         i+=1
       }
     }
-    var n = new SequentialGraph(filtKeys,fKeys,Map(tmpDataKeys.toSeq: _*),dKeys,funcToData,dataToFunc,funcToInputs,nextfkey,nextdkey,runOnModify,WeakReference(this))
+    var n = new SequentialGraph(filtKeys,fKeys,Map(tmpDataKeys.toSeq: _*),dKeys,funcToData,dataToFunc,funcToInputs,nextfkey,nextdkey,runOnModify,WeakReference(this),print)
     n.run(newTodo)
   }
   
@@ -161,11 +163,11 @@ class SequentialGraph (
     val newfuncToInputs: Map[FKey, Vector[DKey]] = funcToInputs ++ g.funcToInputs.filter( p => !fKeys.contains(p._1) )
     val newnextfkey: Int = if(nextfkey > g.nextfkey) nextfkey else g.nextfkey;
     val newnextdkey: Int = if(nextdkey > g.nextdkey) nextdkey else g.nextdkey;
-    return new SequentialGraph(newfiltKeys, newfKeys, newdataKeys, newdKeys, newfuncToData, newdataToFunc,newfuncToInputs, newnextfkey, newnextdkey,runOnModify,WeakReference(this))
+    return new SequentialGraph(newfiltKeys, newfKeys, newdataKeys, newdKeys, newfuncToData, newdataToFunc,newfuncToInputs, newnextfkey, newnextdkey,runOnModify,WeakReference(this),print)
   }
   
   def setRunOnModify(b:Boolean):SequentialGraph={
-    new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, dataToFunc, funcToInputs, nextfkey, nextdkey, b, parent)
+    new SequentialGraph(filtKeys, fKeys, dataKeys, dKeys, funcToData, dataToFunc, funcToInputs, nextfkey, nextdkey, b, parent,print)
   }
   
 }
